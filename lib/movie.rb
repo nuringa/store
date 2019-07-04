c3adee23de0b111b9206d7bde7891da54aa2ed57
admin.karima <admin.karima@gmail.com>
2019-07-04 07:37:04 -0400

    added variables to movie and book classes

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,14 @@
 class Movie < Product
+  attr_reader :title, :year, :director
+  
+  def initialize(params)
+    super
+    @title = params[:title]
+    @year = params[:year]
+    @director = params[:director]
+  end
+  
+  def to_s
+    "Фильм \"#{title}\", #{year}, реж. #{director}, #{super}"
+  end
 end
--- store.rb
+++ store.rb
@@ -2,5 +2,12 @@ require_relative 'lib/product'
 require_relative 'lib/movie'
 require_relative 'lib/book'
 
-movie = Movie.new(price: 290, amount: 12)
-puts "Фильм 'Леон' стоит #{movie.price} руб."
+puts 'Вот такие товары у нас есть:'
+
+products = [
+  Movie.new(title: 'Леон', year: 1994, director: 'Люк Бессон', price: 990, amount: 6),
+  Movie.new(title: 'Дурак', year: 2014, director: 'Юрий Быков', price: 113, amount: 1),
+  Book.new(title: 'Идиот', genre: 'роман', author: 'Федор Достоевский', price: 99, amount: 36)
+  ]
+
+products.each { |product| puts product }
