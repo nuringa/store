76efd0fbb863db2ac985b98b9e758347a2235af9
admin.karima@gmail.com <admin.karima@gmail.com>
2019-07-08 00:53:23 +0300

    fixes after review ver.1

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ShoppingCart
   attr_reader :items, :sum
 
@@ -12,18 +10,6 @@ class ShoppingCart
     @items << item
     item.amount -= 1
     @sum += item.price
-  end
-
-  def show_cart
-    puts 'Вы выбрали:'
-    puts @items.to_a
-    puts "Всего товаров на сумму: #{@sum} руб."
-  end
-
-  def show_total
-    puts 'Вы купили:'
-    puts @items.to_a
-    puts "C Вас #{@sum} руб. Спасибо за покупки!"
   end
 
   def to_a
--- store.rb
+++ store.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'lib/product'
 require_relative 'lib/film'
 require_relative 'lib/book'
@@ -9,24 +7,29 @@ require_relative 'lib/shopping_cart'
 
 collection = ProductCollection.from_dir("#{__dir__}/data/")
 shopping_cart = ShoppingCart.new
-purchase_list = collection.to_a.unshift('Выход')
-
+purchase_list = collection.to_a
 user_choice = -1
 
-until user_choice == 0
+until user_choice.zero?
+  purchase_list.delete_if { |item| item.amount <= 0 }
+  puts purchase_list.map.with_index(1) { |product, i| "#{i}. #{product}" }
+  puts '0. Выход'
+
   puts 'Что хотите купить:'
-
-  purchase_list.delete_if { |item| item.amount <= 0 unless item == 'Выход' }
-  offer = purchase_list.map.with_index { |product, i| "#{i}. #{product}" }
-  puts offer.rotate
-
   user_choice = STDIN.gets.to_i
-  shopping_cart.add_item(collection.products[user_choice]) unless user_choice == 0
-  shopping_cart.show_cart unless user_choice == 0
+
+  unless user_choice.zero?
+    shopping_cart.add_item(collection.products[user_choice - 1])
+    puts 'Вы выбрали:'
+    puts shopping_cart.to_a
+    puts "Всего товаров на сумму: #{shopping_cart.sum} руб."
+  end
 end
 
 if shopping_cart.items.any?
-  shopping_cart.show_total
+  puts 'Вы купили:'
+  puts shopping_cart.to_a
+  puts "C Вас #{shopping_cart.sum} руб. Спасибо за покупки!"
 else
   puts 'Вы ничего не выбрали.'
 end
